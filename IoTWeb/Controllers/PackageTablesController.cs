e847d903d23ae76d305d14cce8f5ea338fbd1627
david750222 <56106698+david750222@users.noreply.github.com>
2019-10-24 15:56:52 +0800

    Package_20191024 1556
    包裹更新中...

---
+++ IoTWeb/Controllers/PackageTablesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IoTWeb.Models;
+
+namespace IoTWeb.Controllers
+{
+    public class PackageTablesController : Controller
+    {
+        private Buliding_ManagementEntities db = new Buliding_ManagementEntities();
+
+        // GET: PackageTables
+        public ActionResult Index()
+        {
+            var packageTable = db.PackageTable.Include(p => p.PackageCompany).Include(p => p.ResidentDataTable).Include(p => p.StaffDataTable).Where(p=>p.Sign==false);
+            return View(packageTable.ToList());
+        }
+
+        // GET: PackageTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PackageTable packageTable = db.PackageTable.Find(id);
+            if (packageTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(packageTable);
+        }
+
+        // GET: PackageTables/Create
+        public ActionResult Create()
+        {
+            ViewBag.PackageCompanyID = new SelectList(db.PackageCompany, "PackageCompanyID", "CompanyName");
+            ViewBag.ReceiverID = new SelectList(db.ResidentDataTable, "ResidentID", "ResidentName");
+            ViewBag.StaffID = new SelectList(db.StaffDataTable, "StaffID", "StaffName");
+            return View();
+        }
+
+        // POST: PackageTables/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PackageID,PackageArrivalDate,PackageCompanyID,Receiver,ReceiverID,Sign,StaffID")] PackageTable packageTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PackageTable.Add(packageTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PackageCompanyID = new SelectList(db.PackageCompany, "PackageCompanyID", "CompanyName", packageTable.PackageCompanyID);
+            ViewBag.ReceiverID = new SelectList(db.ResidentDataTable, "ResidentID", "ResidentName", packageTable.ReceiverID);
+            ViewBag.StaffID = new SelectList(db.StaffDataTable, "StaffID", "StaffName", packageTable.StaffID);
+            return View(packageTable);
+        }
+
+        // GET: PackageTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PackageTable packageTable = db.PackageTable.Find(id);
+            if (packageTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PackageCompanyID = new SelectList(db.PackageCompany, "PackageCompanyID", "CompanyName", packageTable.PackageCompanyID);
+            ViewBag.ReceiverID = new SelectList(db.ResidentDataTable, "ResidentID", "ResidentName", packageTable.ReceiverID);
+            ViewBag.StaffID = new SelectList(db.StaffDataTable, "StaffID", "StaffName", packageTable.StaffID);
+            return View(packageTable);
+        }
+
+        // POST: PackageTables/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PackageID,PackageArrivalDate,PackageCompanyID,Receiver,ReceiverID,Sign,StaffID")] PackageTable packageTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(packageTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PackageCompanyID = new SelectList(db.PackageCompany, "PackageCompanyID", "CompanyName", packageTable.PackageCompanyID);
+            ViewBag.ReceiverID = new SelectList(db.ResidentDataTable, "ResidentID", "ResidentName", packageTable.ReceiverID);
+            ViewBag.StaffID = new SelectList(db.StaffDataTable, "StaffID", "StaffName", packageTable.StaffID);
+            return View(packageTable);
+        }
+
+        // GET: PackageTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PackageTable packageTable = db.PackageTable.Find(id);
+            if (packageTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(packageTable);
+        }
+
+        // POST: PackageTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PackageTable packageTable = db.PackageTable.Find(id);
+            db.PackageTable.Remove(packageTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
