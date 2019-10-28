d5d108b6f8928a73ce323310c06421e9b1e57d31
廖政淳 <56106506+bachikevin@users.noreply.github.com>
2019-10-28 19:32:18 +0800

    管理費v1

---
+++ IoTWeb/Areas/Admin/Models/ManagementFeeMetadata.cs
@@ -0,0 +1,23 @@
+﻿using IoTWeb.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IoTWeb.Models
+{
+    internal class ManagementFeeMetadata
+    {
+        public string RID { get; set; }
+        public int ResidentID { get; set; }
+        public int Fee { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:yyyy-MM-dd}")]
+        [Display(Name ="PaidDate")]
+        public Nullable<System.DateTime> PaidDate { get; set; }
+
+        public virtual ResidentDataTable ResidentDataTable { get; set; }
+
+    }
+}
