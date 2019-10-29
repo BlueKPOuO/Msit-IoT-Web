ced937a787d2b41a4b7cee7693471f2eea664123
廖政淳 <56106506+bachikevin@users.noreply.github.com>
2019-10-29 10:53:43 +0800

    後台-管理費v1.1 穩定版 20191029 10:53
    管理費完成+初步佈告欄

---
+++ IoTWeb/Areas/Admin/Models/BulletinBoardMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IoTWeb.Models
+{
+    internal class BulletinBoardMetadata
+    {
+        internal int No;
+
+        public string StaffID { get; set; }
+        public int annID { get; set; }
+        public string annGrade { get; set; }
+        public string annClass { get; set; }
+        public Nullable<System.DateTime> annDate { get; set; }
+        public string annTitle { get; set; }
+        public string annContent { get; set; }
+        public byte[] annAnnex { get; set; }
+        public string annFilename { get; set; }
+
+        public virtual StaffDataTable StaffDataTable { get; set; }
+
+    }
+}
