495b5c25386b76b6278d25276723839ca3fa365f
廖政淳 <56106506+bachikevin@users.noreply.github.com>
2019-11-12 21:15:05 +0800

    前台 管理費系統 2019-11-12 21:14
    刻版中

---
+++ IoTWeb/Areas/Client/Controllers/ManagementFeesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IoTWeb.Areas.Client.Controllers
+{
+    public class ManagementFeesController : Controller
+    {
+        // GET: Client/ManagementFees
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
