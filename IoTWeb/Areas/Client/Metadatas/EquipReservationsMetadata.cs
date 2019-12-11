69e39ccfbe6cb595712ff892f8a47cb6cff4870a
詹博欽 <56106806+r8211051@users.noreply.github.com>
2019-12-11 12:17:06 +0800

    UI調整

--- IoTWeb/Areas/Client/Metadatas/EquipReservationsMetadata.cs
+++ IoTWeb/Areas/Client/Metadatas/EquipReservationsMetadata.cs
@@ -10,17 +10,17 @@ namespace IoTWeb.Models
         [Display(Name = "設備名稱")]
         public int EquipmentID { get; set; }
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd'T'HH:mm}", ApplyFormatInEditMode = true)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
         [Display(Name = "預約日期")]
         public System.DateTime ReservationDate { get; set; }
         [Display(Name = "住戶名稱")]
-        public int ResidentID { get; set; }
-        /*[DataType(DataType.Date)]       
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "歸還日期")]
-        public Nullable<System.DateTime> ReturnDate { get; set; }*/
+        public int ResidentID { get; set; }       
+        [Display(Name = "租借者")]
         public string Lessee { get; set; }
+        [Display(Name = "租借時數")]
         public int RentTime { get; set; }
+        [Display(Name = "審核")]
         public bool Review { get; set; }
 
         public virtual Equipment Equipment { get; set; }
